9d87a91b39b4b3cfd7bb0b1c911b650317b65f40
이혜진 <4mikemendes@gmail.com>
2021-07-02 09:20:50 +0200

    all buttons working

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let num1 = num1
-let num2 = num2
 
-document.getElementById("munber1-el") = num1
-document.getElementById("munber2-el") = num2
+let num1 = document.getElementById("munber1-el")
+let num2 = document.getElementById("munber2-el")
+
+console.log(num2)
+console.log(num1)
+
+result =  num1 + num2
+console.log(result)
+
 let sum = document.getElementById("sum-el")
 
+function add() {
+  console.log("clikec")
+}
 
-  sum.innerText = num1 + num2
+function sub() {
+  console.log("clikec")
+}
 
+function div() {
+  console.log("clikec")
+}
+
+function mult() {
+  console.log("clikec")
+}
