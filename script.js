2592a41e400a556e01d6d8b90eb73f933340e7e5
이혜진 <4mikemendes@gmail.com>
2021-07-02 08:42:26 +0200

    add

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ let num2 = num2
 
 document.getElementById("munber1-el") = num1
 document.getElementById("munber2-el") = num2
+let sum = document.getElementById("sum-el")
+
+
+  sum.innerText = num1 + num2
 
